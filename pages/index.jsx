daba321e630ed23c1716ccd9eae44ecdfc8058c2
shady41 <abhinavjha2512@gmail.com>
2022-10-26 18:20:24 +0530

    Added title, change favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,15 +2,21 @@ import About from "../components/About"
 import Projects from "../components/Projects"
 import Footer from "../components/Footer"
 import Contact from "../components/Contact"
+import Head from "next/head"
 
 const Home = () => {
   return (
-    <main className="flex w-full flex-col md:px-28 gap-0 md:gap-14">
-      <About/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
-    </main>
+    <>
+      <Head>
+        <title>shady41</title>
+      </Head>
+      <main className="flex w-full flex-col md:px-28 gap-0 md:gap-14">
+        <About/>
+        <Projects/>
+        <Contact/>
+        <Footer/>
+      </main>
+    </>
   )
 }
 
